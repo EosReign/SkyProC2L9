d1debc1f32caec504ae05661ccf39ed8cffa72c5
EosReign <107928418+EosReign@users.noreply.github.com>
2022-10-04 16:17:38 +0300

    Initial commit

---
+++ src/main/java/ProSkySkyProC2L9/SkyProC2L9Application.java
@@ -0,0 +1,13 @@
+package ProSkySkyProC2L9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProC2L9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProC2L9Application.class, args);
+	}
+
+}
---
+++ src/test/java/ProSkySkyProC2L9/SkyProC2L9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProSkySkyProC2L9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProC2L9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
